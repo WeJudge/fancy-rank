3e7f63c3a9b9376150d9aaf2c65de4a06f3a38f2
LanceLRQ <lancelrq@gmail.com>
2020-03-19 22:59:02 +0800

    完善滚榜

--- src/components/item.jsx
+++ src/components/item.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
+import { Avatar, Icon } from 'antd';
+import { get } from 'lodash';
+import classnames from 'classnames';
 import PropTypes from 'prop-types';
+import { UserOutlined } from '@ant-design/icons';
 import ReactSvg from '../images/react.svg'
 
+export const indexToChar = (index) => {
+  const indexParsed = parseInt(index, 10);
+  return (indexParsed > 0 && indexParsed <= 26) ?
+    String.fromCharCode(65 + (indexParsed - 1)) : '-';
+};
 
-class FancyRankItem extends React.PureComponent {
+class FancyRankItem extends React.Component {
   static propTypes = {
     top: PropTypes.number,
     number: PropTypes.number,
-    teamName: PropTypes.string,
-    problems: PropTypes.arrayOf(PropTypes.shape({
-      accepted: PropTypes.number,
-      total: PropTypes.number,
-      hasChange: PropTypes.bool,      // 是否有变化
-      timeuse: PropTypes.number,      // 单位：秒(s)
-    })),
-    solved: PropTypes.number,
-    timeUsed: PropTypes.number,
+    problems: PropTypes.arrayOf(PropTypes.string),
+    problemInfos: PropTypes.shape({}),
+    accountInfo: PropTypes.shape({}),
   };
 
   static defaultProps = {};
@@ -23,33 +26,58 @@ class FancyRankItem extends React.PureComponent {
   state = {};
 
   render() {
+    const { accountInfo, problems, problemInfos } = this.props;
     return <div
       className="fancy-rank-item"
       style={{ top: this.props.top }}
     >
       <div className="rank-item-number">{this.props.number}</div>
       <div className="rank-item-logo">
-        <img src={ReactSvg} width={80} height={80} alt="react" />
+        <Avatar shape="circle" icon={<UserOutlined />} size={64} />
       </div>
       <div className="rank-item-content">
         <div className="rank-item-content-team-name">
-          {this.props.teamName}
+          {get(accountInfo, 'account.nickname')}
         </div>
         <div className="rank-item-content-problems">
-          <div className="rank-item-content-problems-tag">A</div>
-          <div className="rank-item-content-problems-tag accepted">233</div>
-          <div className="rank-item-content-problems-tag failed">-5</div>
-          <div className="rank-item-content-problems-tag changed">-7</div>
-          <div className="rank-item-content-problems-tag">E</div>
-          <div className="rank-item-content-problems-tag">F</div>
-          <div className="rank-item-content-problems-tag">G</div>
+          {problems.map((pid) => {
+            const problem = problemInfos[pid];
+            const submission = get(accountInfo, `solutions.${pid}.submission`, 0);
+            const accepted = get(accountInfo, `solutions.${pid}.accepted`, 0);
+            const penalty = get(accountInfo, `solutions.${pid}.penalty`, 0);
+            const timeUsed = (get(accountInfo, `solutions.${pid}.time_used`, 0) / 60).toFixed(0);
+            const isAccepted = accepted > 0;
+            const hasSubmitted = submission > 0;
+            const classMapping = {
+              accepted: isAccepted,
+              failed: hasSubmitted && !isAccepted,
+            };
+            const buildText = () => {
+              if (classMapping.accepted) {
+                return `${penalty} - ${ timeUsed}`;
+              } else if (classMapping.failed) {
+                return `${penalty}`;
+              }
+              return indexToChar(get(problem, 'order', 0))
+            };
+            return <div className={classnames("rank-item-content-problems-tag", classMapping)}>
+              {buildText()}
+            </div>;
+          })}
+          {/*<div className="rank-item-content-problems-tag">A</div>*/}
+          {/*<div className="rank-item-content-problems-tag accepted">233</div>*/}
+          {/*<div className="rank-item-content-problems-tag failed">-5</div>*/}
+          {/*<div className="rank-item-content-problems-tag changed">-7</div>*/}
+          {/*<div className="rank-item-content-problems-tag">E</div>*/}
+          {/*<div className="rank-item-content-problems-tag">F</div>*/}
+          {/*<div className="rank-item-content-problems-tag">G</div>*/}
         </div>
       </div>
       <div className="rank-item-solved">
-        {this.props.solved}
+        {get(accountInfo, 'solved')}
       </div>
       <div className="rank-item-times">
-        {this.props.timeUsed}
+        {(get(accountInfo, 'time_used') / 60).toFixed(0)}
       </div>
     </div>;
   }
--- src/fancy_rank.jsx
+++ src/fancy_rank.jsx
@@ -1,16 +1,68 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import faker from 'faker';
+import { get } from 'lodash';
+import resp from './data.json';
 import './styles/fancy-rank.scss';
 import FancyRankItem from './components/item';
 
-class FancyRank extends React.PureComponent {
-  static propTypes = {};
-  static defaultProps = {};
-  state = {};
+const ITEM_COMMON_HEIGHT = 80;
+
+class FancyRank extends React.Component {
+  static propTypes = {
+    fancyRankData: PropTypes.shape({})
+  };
+  static defaultProps = {
+    fancyRankData: resp.data,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentIndex: 0,
+      ...this.initState(),
+    };
+    console.log(this.state);
+  }
+
+  state = {
+
+  };
+
+  initState = () => {
+    const rankListRaw = JSON.parse(JSON.stringify(get(this.props.fancyRankData, 'ranklist', [])));
+    const problemListRaw = JSON.parse(JSON.stringify(get(this.props.fancyRankData, 'problems', [])));
+    const rankList = [];
+    const accountInfos = {};
+    const problems = [];
+    const problemInfos = {};
+    // 执行normalizr操作
+    rankListRaw.forEach((item) => {
+      const accountId = get(item, 'account.id');
+      rankList.push(accountId);
+      accountInfos[accountId] = item;
+    });
+    problemListRaw.sort((a, b) => a.order > b.order);
+    problemListRaw.forEach((item) => {
+      const problemId = get(item, 'problem_id');
+      problems.push(problemId);
+      problemInfos[problemId] = item;
+    });
+    return {
+      rankList,
+      accountInfos,
+      problems,
+      problemInfos,
+      currentIndex: 0,
+    }
+  };
+
+  setPosition = (from, to) => {
+    const { indexMapping } = this.state;
+
+
+  };
 
   render() {
-    const items = Array.call(null, ...Array(24)).map((_, h) => h);
     return <div className="fancy-rank">
       <div className="fancy-rank-header">
         <div className="fancy-rank-header-item rank">排名</div>
@@ -19,14 +71,14 @@ class FancyRank extends React.PureComponent {
         <div className="fancy-rank-header-item time">时间</div>
       </div>
       <div className="fancy-rank-warp">
-        {items.map((i) => {
+        {this.state.rankList.map((accountId, index) => {
           return <FancyRankItem
-            key={i}
-            top={i * 80}
-            number={i + 1}
-            teamName={faker.name.findName()}
-            solved={faker.random.number({min: 0, max: 6})}
-            timeUsed={faker.random.number({min: 0, max: 999})}
+            key={accountId}
+            top={index * ITEM_COMMON_HEIGHT}
+            number={index + 1}
+            problems={this.state.problems}
+            problemInfos={this.state.problemInfos}
+            accountInfo={this.state.accountInfos[accountId]}
           />;
         })}
       </div>
