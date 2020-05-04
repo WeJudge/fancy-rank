71693ab867cee3e248ec8b109d6c2c4a96424388
LanceLRQ <lancelrq@gmail.com>
2020-05-04 20:35:32 +0800

    更新样式

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import FancyRank from './fancy_rank';
-import bg from './images/beach-1852945_1920.jpg';
 import './index.scss';
 
 function App() {
   return (
-    <div style={{ backgroundImage: `url(${bg})` }} className="fake_background">
+    <div className="fake_background">
       <FancyRank />
     </div>
   );
